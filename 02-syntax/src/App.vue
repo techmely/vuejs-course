16ed2aa416d0d6d26c944e41d12b493607d2ed6f
hoapn-fsoft <thaycacac.official@gmail.com>
2024-02-01 16:39:24 +0700

    binding with method

--- 02-syntax/src/App.vue
+++ 02-syntax/src/App.vue
@@ -3,7 +3,7 @@ const message = 'hello world!!'
 const rawHtml = '<div><h1>Title</h1><span style="color: red">This should be red.</span></div>'
 const buttonId = 'button-id'
 const buttonClass = 'btn btn-rounded'
-const isButtonDisabled = true
+const isButtonDisabled = false
 const objectOfAttrs = {
   id: 'container',
   class: 'wrapper',
@@ -11,12 +11,22 @@ const objectOfAttrs = {
     color: 'blue'
   }
 }
+const number = 2
+const listString = 'a,b,c,d,e,f,g,h,i,j'
+const convertToId = (data) => data.split(',').reverse().join('-')
+
+const attributeName = 'href'
+const url = 'http://kungfutech.edu.vn'
 </script>
 
 <template>
   <h1>Syntax</h1>
   <p>Message: {{ message }}</p>
   <span v-html="rawHtml"></span>
-  <button :id="buttonId" :class="buttonClass" :disabled="isButtonDisabled">Click here</button>
+  <button :id="buttonId" :class="buttonClass" :disabled="isButtonDisabled">
+    {{ isButtonDisabled ? 'Disabled' : 'Click here' }}
+  </button>
   <button v-bind="objectOfAttrs">Click here!!!</button>
+  <button :id="convertToId(listString)">Number: {{ number * 2 }}</button>
+  <a v-bind:[attributeName]="url">Click here</a>
 </template>
