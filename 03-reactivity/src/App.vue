01a8cc7c440c232e1956c1d37322280c997721bb
hoapn-fsoft <thaycacac.official@gmail.com>
2024-02-17 13:53:43 +0700

    feat: add ref

---
+++ 03-reactivity/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(2)
+count.value++
+
+const increase = () => count.value++
+</script>
+
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button @click="increase">Increase</button>
+  </div>
+</template>
