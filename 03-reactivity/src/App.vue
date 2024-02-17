28cd8861e9671867eb3efb6e1a0ff64ad505e662
hoapn-fsoft <thaycacac.official@gmail.com>
2024-02-17 15:45:56 +0700

    feat: update reactive

--- 03-reactivity/src/App.vue
+++ 03-reactivity/src/App.vue
@@ -1,15 +1,26 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
-const count = ref(2)
-count.value++
+const object = reactive({
+  count: 2,
+  user: {
+    age: 18
+  },
+  array: [1, 2, 3]
+})
 
-const increase = () => count.value++
+const change = () => {
+  object.count++
+  object.user.age++
+  object.array.push(4)
+}
 </script>
 
 <template>
   <div>
-    <p>{{ count }}</p>
-    <button @click="increase">Increase</button>
+    <p>Count: {{ object.count }}</p>
+    <p>Age: {{ object.user.age }}</p>
+    <p>Array: {{ object.array }}</p>
+    <button @click="change">Increase</button>
   </div>
 </template>
