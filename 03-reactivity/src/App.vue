7eaa06ec4e8797fb5c62643356d7cc7519f69530
hoapn-fsoft <thaycacac.official@gmail.com>
2024-02-17 16:24:24 +0700

    feat: add next tick

--- 03-reactivity/src/App.vue
+++ 03-reactivity/src/App.vue
@@ -1,26 +1,19 @@
 <script setup>
-import { reactive } from 'vue'
+import { nextTick, ref } from 'vue'
 
-const object = reactive({
-  count: 2,
-  user: {
-    age: 18
-  },
-  array: [1, 2, 3]
-})
+const count = ref(0)
 
-const change = () => {
-  object.count++
-  object.user.age++
-  object.array.push(4)
+const increase = async () => {
+  count.value++
+  console.log(document.getElementById('count').innerText)
+  await nextTick()
+  console.log(document.getElementById('count').innerText)
 }
 </script>
 
 <template>
   <div>
-    <p>Count: {{ object.count }}</p>
-    <p>Age: {{ object.user.age }}</p>
-    <p>Array: {{ object.array }}</p>
-    <button @click="change">Increase</button>
+    <p id="count">Count: {{ count }}</p>
+    <button @click="increase">Increase</button>
   </div>
 </template>
