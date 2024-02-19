75cb8a143ac424d9177d552f04760c0934787bcb
hoapn-fsoft <thaycacac.official@gmail.com>
2024-02-19 21:57:02 +0700

    feat: update computed

---
+++ 04-computed/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, reactive } from 'vue'
+
+const book = reactive({
+  name: 'Book 1',
+  chapters: []
+})
+
+const addChapter = () => { 
+  book.chapters.push('Chapter')
+}
+
+const isPublished = computed(() => {
+  return book.chapters.length > 0 ? 'Có' : 'Không'
+})
+</script>
+
+<template>
+  <div>
+    <p id="count">Book name: {{ book.name }}</p>
+    <p id="count">Đã xuất bản: {{ isPublished }}</p>
+    <button @click="addChapter">Add chapter</button>
+  </div>
+</template>
