e4a35bff33ec7f97d1ed82ba2ba6d8916b4cde81
hoapn-fsoft <thaycacac.official@gmail.com>
2024-03-30 16:33:18 +0700

    feat: using v-for

---
+++ 07-list/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, reactive } from "vue";
+const shoppingItems = ref([
+  {
+    name: "apple",
+    price: 7,
+  },
+  {
+    name: "orange",
+    price: 12,
+  },
+  {
+    name: "banana",
+    price: 10,
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="item in shoppingItems" :key="item.name">
+        {{ item.name }} - {{ item.price }}
+      </li>
+    </ul>
+    <span v-for="n in 10" :key="n">{{ n }}</span>
+  </div>
+</template>
