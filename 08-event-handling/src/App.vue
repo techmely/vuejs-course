7c26da16c63651f6a586a4b79a5d7987f343da7b
hoapn-fsoft <thaycacac.official@gmail.com>
2024-04-20 16:10:53 +0700

    feat: add event handling

---
+++ 08-event-handling/src/App.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/no-use-v-if-with-v-for -->
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const greet = (event) => {
+  console.log(event.target);
+  alert(`Xin chào ${event.type}`);
+};
+
+const sayHi = (name) => {
+  alert(`Xin chào ${name}`);
+};
+</script>
+
+<template>
+  <div>
+    <h1>Event handling</h1>
+    <button @click="count++">Thêm 1</button>
+    <p>{{ count }}</p>
+    <button @click="greet">Chào</button>
+    <button @click="sayHi('A')">Chào A</button>
+    <button @click="sayHi('B')">Chào B</button>
+  </div>
+</template>
