5d0a8df0494ffcda51be00462be5ad99bcb7721b
hoapn-fsoft <thaycacac.official@gmail.com>
2024-04-20 16:26:01 +0700

    feat: event argument

--- 08-event-handling/src/App.vue
+++ 08-event-handling/src/App.vue
@@ -3,23 +3,28 @@
 import { ref } from "vue";
 const count = ref(0);
 
-const greet = (event) => {
-  console.log(event.target);
-  alert(`Xin chào ${event.type}`);
+const sayHi1 = () => {
+  alert(`Xin chào 1`);
 };
 
-const sayHi = (name) => {
-  alert(`Xin chào ${name}`);
+const sayHi2 = () => {
+  alert(`Xin chào 2`);
+};
+
+const submit = () => {
+  alert("Form submitted");
 };
 </script>
 
 <template>
   <div>
     <h1>Event handling</h1>
-    <button @click="count++">Thêm 1</button>
-    <p>{{ count }}</p>
-    <button @click="greet">Chào</button>
-    <button @click="sayHi('A')">Chào A</button>
-    <button @click="sayHi('B')">Chào B</button>
+    <button @click.self="sayHi1">
+      <button @click.self="sayHi2">say hi 2</button>
+      say hi 1
+    </button>
+    <form @submit.prevent="submit">
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
