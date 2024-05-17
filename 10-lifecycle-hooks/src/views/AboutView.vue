1465a6955d0778a32c61008c5fd90a5573d9399d
hoapn-fsoft <thaycacac.official@gmail.com>
2024-05-17 20:50:06 +0700

    feat: add lifecycle hooks

---
+++ 10-lifecycle-hooks/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>Timer: {{ time }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted, ref } from "vue";
+
+const time = ref(0);
+let intervalId = null;
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+
+onMounted(() => {
+  console.log("onMounted");
+  intervalId = setInterval(() => {
+    time.value++;
+  }, 1000);
+});
+
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount");
+  clearInterval(intervalId);
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
+</script>
