8a7569c0bd4675be02ebc67aaf78fe2788ea1da7
hoapn-fsoft <thaycacac.official@gmail.com>
2024-05-24 17:16:25 +0700

    feat: update refs with v-for

--- 12-template-refs/src/App.vue
+++ 12-template-refs/src/App.vue
@@ -1,15 +1,27 @@
 <script setup>
 import { onMounted, ref } from "vue";
 const inputRef = ref(null);
+const list = ref(["Item 1", "Item 2", "Item 3"]);
+const itemRefs = ref([]);
 onMounted(() => {
   inputRef.value.focus();
+
+  itemRefs.value.forEach((item) => {
+    item.style.transition = "opacity 5s";
+    item.style.opacity = 1;
+  });
 });
-console.log(inputRef)
 </script>
 
 <template>
   <div>
     <h1>Template refs</h1>
     <input ref="inputRef" />
+
+    <ul>
+      <li v-for="(item, index) in list" :key="index" ref="itemRefs" style="opacity: 0">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
