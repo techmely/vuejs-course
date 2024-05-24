927d8f997921c145b7122249ee10317f40742db8
hoapn-fsoft <thaycacac.official@gmail.com>
2024-05-24 17:03:25 +0700

    feat: template refs

---
+++ 12-template-refs/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted, ref } from "vue";
+const inputRef = ref(null);
+onMounted(() => {
+  inputRef.value.focus();
+});
+console.log(inputRef)
+</script>
+
+<template>
+  <div>
+    <h1>Template refs</h1>
+    <input ref="inputRef" />
+  </div>
+</template>
