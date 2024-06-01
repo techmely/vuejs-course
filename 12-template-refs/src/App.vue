bc74df8391c8448fd14e55f08427b4e598364ae0
hoapn-fsoft <thaycacac.official@gmail.com>
2024-06-01 09:54:28 +0700

    feat: ref in vue

--- 12-template-refs/src/App.vue
+++ 12-template-refs/src/App.vue
@@ -4,22 +4,37 @@ const inputRef = ref(null);
 const list = ref(["Item 1", "Item 2", "Item 3"]);
 const itemRefs = ref([]);
 onMounted(() => {
-  inputRef.value.focus();
-
+  // inputRef.value.focus();
+  console.log(itemRefs.value)
   itemRefs.value.forEach((item) => {
     item.style.transition = "opacity 5s";
     item.style.opacity = 1;
   });
 });
+
+const initializeThirdPartyLibrary = (el) => {
+  console.log(el);
+};
 </script>
 
 <template>
   <div>
     <h1>Template refs</h1>
-    <input ref="inputRef" />
+    <input
+      :ref="
+        (el) => {
+          initializeThirdPartyLibrary(el);
+        }
+      "
+    />
 
     <ul>
-      <li v-for="(item, index) in list" :key="index" ref="itemRefs" style="opacity: 0">
+      <li
+        v-for="(item, index) in list"
+        :key="index"
+        :ref="(el) => (itemRefs[index] = el)"
+        style="opacity: 0"
+      >
         {{ item }}
       </li>
     </ul>
