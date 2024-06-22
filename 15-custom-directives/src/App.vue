a83a5394ccc61181ce98e875883e34a40ed05ef9
hoapn-fsoft <thaycacac.official@gmail.com>
2024-06-22 16:29:28 +0700

    hooks for custom directives

--- 15-custom-directives/src/App.vue
+++ 15-custom-directives/src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
-// const vFocus = {
-//   mounted: (el) => el.focus()
-// }
+const vColor = {
+  mounted: (el, binding) => {
+    el.style.color = binding.value;
+  },
+};
+
+const vDemo = {
+  mounted: (el, binding) => {
+    el.style.color = binding.value.color;
+    el.style.background = binding.value.background;
+  },
+};
 </script>
 
 <template>
   <div>
-    <h1>Custom Directives</h1>
-    <input v-focus placeholder="Input your name"/>
+    <h1
+      v-demo="{
+        color: 'red',
+        background: 'blue',
+      }"
+    >
+      Custom Directives
+    </h1>
+    <input v-focus placeholder="Input your name" />
   </div>
 </template>
