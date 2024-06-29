ac5f5190dd74b38a74200ae7037f9d6d429b7596
hoapn-fsoft <thaycacac.official@gmail.com>
2024-06-29 14:18:55 +0700

    feat : update plugins

---
+++ 16-plugins/src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1
+      v-demo="{
+        color: 'red',
+        background: 'blue',
+      }"
+    >
+      Plugins
+    </h1>
+    <p>{{ $translate('greetings.hello') }}</p>
+    <p>{{ $translate('greetings.message') }}</p>
+  </div>
+</template>
