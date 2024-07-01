9417df79bc9124520845c09376ebb2b14ab67b74
hoapn-fsoft <thaycacac.official@gmail.com>
2024-07-01 21:36:56 +0700

    feat: transitions

---
+++ 17-transitions/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+const isShow = ref(true);
+</script>
+
+<template>
+  <div>
+    <h1>Transition</h1>
+    <button @click="isShow = !isShow">Toggle fade</button>
+    <Transition>
+      <p v-show="isShow">Hello</p>
+    </Transition>
+  </div>
+</template>
+
+<style lang="css">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
